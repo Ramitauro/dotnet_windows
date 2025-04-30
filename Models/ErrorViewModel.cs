3b7331cde60445eee34ff3b6c1b9a0ed0ffae5cf
Ramit Singh <Ramit.Singh@aurosociety.org>
2025-04-30 16:40:08 +0530

    Add ASP.NET Core 5 NLog example

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NetCore5_NLog_Web_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
